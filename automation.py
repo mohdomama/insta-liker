7bca6d2007c4fd42867332f9c68c42bfaf4772d1
Mohd Omama <mohd.omama@gmail.com>
2017-08-14 12:21:17 +0530

    Change xpath for better element location. Imroved exception handling for incorrect username/password

--- automation.py
+++ automation.py
@@ -6,11 +6,12 @@ import getpass,sys
 
 
 def setup():
+   
     global browser
     driverPath = os.getcwd()+'/chromedriver'
     url = r'https://www.instagram.com/accounts/login/'
     chromeOptions = webdriver.ChromeOptions()
-    chromeOptions.binary_location='/opt/google/chrome/google-chrome'
+    #chromeOptions.binary_location='/opt/google/chrome/google-chrome'
 
     '''                                             #These arguments make chrome run headless.Unfortunately the chrome headless is in beta and hence considerably slow.
     chromeOptions.add_argument("--headless")            
@@ -20,8 +21,9 @@ def setup():
 
     prefs = {"profile.managed_default_content_settings.images":2}
     chromeOptions.add_experimental_option("prefs",prefs)
-
+    print('reached 1')
     browser = webdriver.Chrome(driverPath,chrome_options=chromeOptions)
+    print('reached')
     #browser.set_window_position(-10000000, 0)       #move chrome away from view
     print('Fetching login page..')
     browser.get(url)
@@ -44,34 +46,44 @@ def login(username,password):
     while True:
         if browser.current_url=='https://www.instagram.com/':
             break
-        elif time.time()-loginTime>5:
-            try:
-                element=browser.find_element_by_class_name('_q90d5')
-                print(element.text)
-                print('\n\nEnter your credentials again!\n')
-                browser.close()
-                main()
-                sys.exit()
-            except Exception as e:
+        elif time.time()-loginTime>10:                  #if login takes more than 10 seconds, tell user that his internet is slow
                 print('Your Internet seems slow!\n',e)
                 browser.close()
                 sys.exit()
-
+        else:
+            try:
+                element=browser.find_element_by_xpath("//*[@id='slfErrorAlert']")
+                print('\n'+element.text)
+                print('Enter your credentials again!\n')
+                browser.close()
+                main()
+                sys.exit()
+            except:
+                #do nothing
+                pass
 
     print('login complete')
 
 def scroll(number_posts):
-    
-    scroll_xpath=r"(//*[@class='_tk4ba _1tv0k'])["+number_posts+"]"
+    scroll_xpath=r"//span[text()='Like' or text()='Unlike']"
     print('Scrolling..')
 
     while True:
         try:
-            element = browser.find_element_by_xpath(scroll_xpath)
-            print("Reached")
-            break
+            try:
+                posts=len(browser.find_elements_by_xpath(scroll_xpath))
+            except:
+                posts = 0
+            
+            print(posts)
 
-        except:
+            if(posts>=int(number_posts)):
+                break
+            else:
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        except Exception as e:
+            print("Reached Exceptio\n",e)
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             
 
@@ -80,7 +92,7 @@ def scroll(number_posts):
 def like(number_posts):
     likeNumber=0
     print('Liking the posts..')
-    elements = browser.find_elements_by_xpath("//*[@class='_tk4ba _1tv0k']")
+    elements = browser.find_elements_by_xpath(r"//span[text()='Like' or text()='Unlike']")
     for i in range (int(number_posts)):
         if elements[i].text=='Like':
             print('Liked')
