9a2e4e43f07d847b12af865b43dc46e6b224512c
Mohd Omama <mohd.omama@gmail.com>
2018-01-02 15:01:28 +0530

    Change webdriver. Change some basic methods.
    Changes Done in methods :
    - Switched to firefox. Add geckodriver in the repository for the same.
    - Liking and scrolling takes place at same time (one element at a time)
    - The program now likes top <number_post> unliked posts
    - Linting

--- automation.py
+++ automation.py
@@ -1,37 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
 import os, time 
 import getpass,sys
 
 
 def setup():
-   
     global browser
-    driverPath = os.getcwd()+'/chromedriver'
+    PATH = os.environ['PATH']
+    pwd = os.getcwd()
+    os.environ['PATH'] = PATH + ':' + pwd
+
     url = r'https://www.instagram.com/accounts/login/'
-    chromeOptions = webdriver.ChromeOptions()
-    #chromeOptions.binary_location='/opt/google/chrome/google-chrome'
 
-    '''                                             #These arguments make chrome run headless.Unfortunately the chrome headless is in beta and hence considerably slow.
-    chromeOptions.add_argument("--headless")            
-    chromeOptions.add_argument("--disable-gpu")
-    chromeOptions.add_argument("--start-fullscreen")
-    '''
+    firefoxProfile = FirefoxProfile()
+    firefoxProfile.set_preference('permissions.default.stylesheet', 2)
+    firefoxProfile.set_preference('permissions.default.image', 2)
+    firefoxProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so',
+                                  'false') 
 
-    prefs = {"profile.managed_default_content_settings.images":2}
-    chromeOptions.add_experimental_option("prefs",prefs)
-    print('reached 1')
-    browser = webdriver.Chrome(driverPath,chrome_options=chromeOptions)
-    print('reached')
+    browser = webdriver.Firefox(firefoxProfile)
     #browser.set_window_position(-10000000, 0)       #move chrome away from view
     print('Fetching login page..')
     browser.get(url)
-    print('reached login page')    
+    print('reached login page')   
+
 
 def login(username,password):
     print('logging in..')
-    element = browser.find_element_by_name('username')
+    while True :
+        try:
+            element = browser.find_element_by_name('username')
+            break
+        except:
+            pass
     element.clear()
     element.send_keys(username)
 
@@ -45,8 +48,11 @@ def login(username,password):
     loginTime=time.time()
     while True:
         if browser.current_url=='https://www.instagram.com/':
+            time.sleep(2)
             break
-        elif time.time()-loginTime>10:                  #if login takes more than 10 seconds, tell user that his internet is slow
+
+        #if login takes more than 10 seconds, tell user that his internet is slow
+        elif time.time()-loginTime>10:                 
                 print('Your Internet seems slow!\n',e)
                 browser.close()
                 sys.exit()
@@ -59,51 +65,24 @@ def login(username,password):
                 main()
                 sys.exit()
             except:
-                #do nothing
                 pass
 
     print('login complete')
+            
 
-def scroll(number_posts):
-    scroll_xpath=r"//span[text()='Like' or text()='Unlike']"
+def scroll_and_like(number_posts):
+    scroll_xpath=r"//span[text()='Like']"
     print('Scrolling..')
-
-    while True:
-        try:
-            try:
-                posts=len(browser.find_elements_by_xpath(scroll_xpath))
-            except:
-                posts = 0
-            
-            print(posts)
-
-            if(posts>=int(number_posts)):
-                break
-            else:
-                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+    count = 0
+    while (count < int(number_posts)):
+        try :
+            element = browser.find_element_by_xpath(scroll_xpath)
+            element.click()
+            count+=1
+            print ('Liked! Like count :', count)
         except Exception as e:
-            print("Reached Exceptio\n",e)
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            
-
-
-
-def like(number_posts):
-    likeNumber=0
-    print('Liking the posts..')
-    elements = browser.find_elements_by_xpath(r"//span[text()='Like' or text()='Unlike']")
-    for i in range (int(number_posts)):
-        if elements[i].text=='Like':
-            print('Liked')
-            likeNumber+=1
-            try:
-                elements[i].click()
-            except:
-                print('some excetion for ',i)
-
-    
-    print('Liking Finished!!\n{} posts liked!'.format(likeNumber))
+        
 
 def main():
     
@@ -114,8 +93,8 @@ def main():
     start_time = time.time()
     setup()
     login(username,password)
-    scroll(number_posts)
-    like(number_posts)
+    scroll_and_like(number_posts)
+    #like(number_posts)
     print('Time taken = ',time.time()-start_time)
     browser.close()
 
